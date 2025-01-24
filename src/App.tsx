fcaaf00bc7ee2a7b4028316875f311d19aaada3b
Kiet Huynh <kiet.huynh@mercatus.com>
2025-01-24 10:56:44 +0700

    reduce code size for build

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { persistor, store } from './redux';
-import '@/utils/prototype';
+import './utils/prototype';
 import { PersistGate } from 'redux-persist/integration/react';
 
 export default function App() {
--- src/utils/prototype.ts
+++ src/utils/prototype.ts
@@ -2,40 +2,40 @@ import dayjs from 'dayjs';
 
 //@ts-ignore
 String.prototype.truncate = function (num: number) {
-    if (this.length > num) {
-        return (this.slice(0, num) + '...') as string;
-    } else {
-        return this as string;
-    }
+  if (this.length > num) {
+    return (this.slice(0, num) + '...') as string;
+  } else {
+    return this as string;
+  }
 };
 
 //@ts-ignore
 String.prototype.prettyMoney = function (hideSymbol?: boolean) {
-    let formatter = new Intl.NumberFormat('en-US', {
-        style: 'currency',
-        currency: 'VND',
-    });
-    return formatter.format(Number(this));
+  let formatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'VND',
+  });
+  return formatter.format(Number(this));
 };
 
 //@ts-ignore
 String.prototype.prettyDate = function () {
-    return dayjs(String(this)).format('DD/MM/YYYY');
+  return dayjs(String(this)).format('DD/MM/YYYY');
 };
 
 //@ts-ignore
 String.prototype.prettyDateTime = function () {
-    return dayjs(String(this)).format('DD/MM/YYYY - hh:mm:ss');
+  return dayjs(String(this)).format('DD/MM/YYYY - hh:mm:ss');
 };
 
 //@ts-ignore
 Array.prototype.has = function (item) {
-    if (!isEqualNoop) throw new Error('Missing isEqualKey!');
-    return this.some((currentItem) => isEqualNoop(item, currentItem));
+  if (!isEqualNoop) throw new Error('Missing isEqualKey!');
+  return this.some((currentItem) => isEqualNoop(item, currentItem));
 };
 
 const isEqualNoop = (a: any, b: any) => {
-    return a?.id === b?.id;
+  return a?.id === b?.id;
 };
 
-export {isEqualNoop as isEqual};
+export { isEqualNoop as isEqual };
